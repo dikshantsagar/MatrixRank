551a46cc8575a112dc22eb070a90ad1b80530b2c
Dikshant Sagar <31593366+dikshantsagar@users.noreply.github.com>
2017-10-24 22:20:30 +0530

    Add files via upload

--- a4.py
+++ a4.py
@@ -26,4 +26,21 @@ def Row_Transformation(a,x,row1,row2):
 
 def matrixrank(a):
 	
+	rank=3
+	for k in range(3):
+		if a[k][k]!=0:
+
+
+		elif a[k][k]==0:
+
+			if a[k+1][0]==0 and a[k+1][1]==0 and a[k+1][0]:
+				a=swapRows(a,k,2)
+				rank-=1
+
+			elif a[k+1][k]==0:
+				a=swapRows(a,k,k+1)
+
+
+
+
 
