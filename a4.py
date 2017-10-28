d7889f5d7e334fc2cb39a351f9ac4bcdb1c18dc7
Dikshant Sagar <31593366+dikshantsagar@users.noreply.github.com>
2017-10-28 11:57:33 +0530

    Update a4.py

--- a4.py
+++ a4.py
@@ -27,20 +27,37 @@ def Row_Transformation(a,x,row1,row2):
 def matrixrank(a):
 	
 	rank=3
+
 	for k in range(3):
-		if a[k][k]!=0:
+		while k <=rank-1:
+
+			if a[k][k] is not 0:
+				if k=0:
+			
+					fac1=a[0][0]/a[0][1]
+					a=Row_Transformation(a,-fac1,0,1)
+					fac2=a[0][0]/a[0][2]
+					a=Row_Transformation(a,-fac2,0,2)
+
+				elif k=1:
+					
+
+				elif k=2:
 
 
-		elif a[k][k]==0:
 
-			if a[k+1][0]==0 and a[k+1][1]==0 and a[k+1][0]:
-				a=swapRows(a,k,2)
-				rank-=1
+			elif a[k][k]==0:
 
-			elif a[k+1][k]==0:
-				a=swapRows(a,k,k+1)
+				if a[k+1][0]==0 and a[k+1][1]==0 and a[k+1][0]:
+					a=swapRows(a,k,2)
+					rank-=1
 
+				elif a[k+1][k]==0:
+					a=swapRows(a,k,k+1)
 
+				k-=1
+
+	return rank
 
 
 
